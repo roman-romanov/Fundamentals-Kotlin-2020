d5b14bf0b46ac6f36be46c8d71520ccb4d4e52fd
Roman <designer_79@mail.ru>
2020-12-07 19:54:21 +0300

    homework2

--- app/src/main/java/com/example/fundamentals_kotlin_2020/MainActivity.kt
+++ app/src/main/java/com/example/fundamentals_kotlin_2020/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.fundamentals_kotlin_2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
     }
 }
---
+++ app/src/main/java/com/example/fundamentals_kotlin_2020/MovieDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.fundamentals_kotlin_2020
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
